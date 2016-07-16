6f70d343c6604bf97e32c63d19f9837f1d98871b
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-07-16 02:57:30 +0200

    The pattern could be defined as an array of integer values

--- NanoPlayBoard/examples/05. ledmatrix/printPattern/printPattern.ino
+++ NanoPlayBoard/examples/05. ledmatrix/printPattern/printPattern.ino
@@ -3,10 +3,10 @@
 NanoPlayBoard board;
 
 void setup() {
-
 }
 
 void loop() {
+  /*
   byte pattern[5] = {
     0b00010000,
     0b01001000,
@@ -14,6 +14,7 @@ void loop() {
     0b01001000,
     0b00010000
     };
-
+  */
+  byte pattern[5] = {16, 72, 8, 72, 16};
   board.ledmatrix.print(pattern);  
 }
