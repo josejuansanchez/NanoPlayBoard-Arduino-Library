e55fd396eeaf97e08d1acb4d5a08c12e74b96e51
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-08-27 21:16:38 +0200

    Added MIDI example using the LDR

---
+++ NanoPlayBoard/examples/07. midi/midi_note_ldr/midi_note_ldr.ino
@@ -0,0 +1,42 @@
+#include <NanoPlayBoard.h>
+
+NanoPlayBoard board;
+
+// timer variables
+unsigned long currentMillis;
+unsigned long previousMillis;
+
+// how often to run the main loop (in ms)
+int samplingInterval = 19;
+
+int pitch;
+int previousPitch;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void noteOn(int cmd, int pitch, int velocity) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+}
+
+void loop() {
+  currentMillis = millis();
+  if (currentMillis - previousMillis > samplingInterval) {
+    previousMillis += samplingInterval;
+    
+    int potVal = board.ldr.read();
+    pitch = map(potVal, 0, 1023, 0, 127);
+
+    if (pitch == previousPitch) return;
+    noteOn(0x90, previousPitch, 0x00);
+    previousPitch = pitch;
+  
+    noteOn(0x90, pitch, 0x6E);
+  }
+
+  board.rgb.write(pitch, pitch, pitch);
+  board.ledmatrix.print(pitch);
+}
