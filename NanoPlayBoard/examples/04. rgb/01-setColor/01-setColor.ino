63f6c986166c313d7084acaa01e07b99730c8d6b
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-08-27 22:53:44 +0200

    Renamed folders

--- NanoPlayBoard/examples/04. rgb/01-setColor/01-setColor.ino
+++ NanoPlayBoard/examples/04. rgb/01-setColor/01-setColor.ino
@@ -7,10 +7,10 @@ void setup() {
 }
 
 void loop() {
-  board.rgb.write(255, 0, 0);  
+  board.rgb.setColor(255, 0, 0);  
   delay(500);
-  board.rgb.write(0, 255, 0);
+  board.rgb.setColor(0, 255, 0);
   delay(500);
-  board.rgb.write(0, 0, 255);  
+  board.rgb.setColor(0, 0, 255);  
   delay(500);
 }
