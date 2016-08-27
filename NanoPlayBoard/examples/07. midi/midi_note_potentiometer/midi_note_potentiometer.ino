010608d3f46c88b6a5ff18d3ad6700545b5534cc
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-08-27 13:32:34 +0200

    Added a new example to use the board as a MIDI controller

---
+++ NanoPlayBoard/examples/07. midi/midi_note_potentiometer/midi_note_potentiometer.ino
@@ -0,0 +1,49 @@
+#include <NanoPlayBoard.h>
+
+NanoPlayBoard board;
+
+// timer variables
+unsigned long currentMillis;
+unsigned long previousMillis;
+
+// how often to run the main loop (in ms)
+int samplingInterval = 19;
+
+int pitch;
+int previousPitch;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void noteOn(int cmd, int pitch, int velocity) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+}
+
+void loop() {
+  currentMillis = millis();
+  if (currentMillis - previousMillis > samplingInterval) {
+    previousMillis += samplingInterval;
+    
+    int potVal = board.potentiometer.read();
+    pitch = map(potVal, 0, 1023, 0, 127);
+
+    if (pitch == previousPitch) return;
+    noteOn(0x90, previousPitch, 0x00);
+    previousPitch = pitch;
+  
+    noteOn(0x90, pitch, 0x6E);
+  }
+
+  if (pitch <= 40) {
+    board.rgb.write(255, 0, 0);
+  } else if (pitch <= 80) {
+    board.rgb.write(0, 255, 0);
+  } else {
+    board.rgb.write(0, 0, 255);
+  }
+  
+  board.ledmatrix.print(pitch);
+}
