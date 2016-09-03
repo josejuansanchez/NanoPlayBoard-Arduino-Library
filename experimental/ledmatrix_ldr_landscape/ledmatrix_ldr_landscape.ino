02eed830fe7a972689bb226bfb46586b414575a0
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-09-03 13:11:09 +0200

    Added example showing the ldr values in landscape mode

---
+++ experimental/ledmatrix_ldr_landscape/ledmatrix_ldr_landscape.ino
@@ -0,0 +1,13 @@
+#include <NanoPlayBoard.h>
+
+NanoPlayBoard board;
+
+void setup() {
+    
+}
+
+void loop() {
+  int potValue = board.ldr.read();
+  int number = map(potValue, 0, 1022, 0, 99);
+  board.ledmatrix.printInLandscape(number);
+}
