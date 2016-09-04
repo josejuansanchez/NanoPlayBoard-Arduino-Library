137fd8027bae5286e41386d2583ae0c53a0959ae
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-09-04 03:04:48 +0200

    Added new example for the scaleTo method

---
+++ examples/02. ldr/scaleTo/scaleTo.ino
@@ -0,0 +1,12 @@
+#include <NanoPlayBoard.h>
+
+NanoPlayBoard board;
+
+void setup() {
+
+}
+
+void loop() {
+  int ldrValue = board.ldr.scaleTo(0, 99);
+  board.ledmatrix.printInLandscape(ldrValue);
+}
