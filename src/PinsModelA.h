b73615a58da975ae205b4d19cbc17533739c96f1
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-09-15 03:01:35 +0200

    Added files with the pin values used for both boards

---
+++ src/PinsModelA.h
@@ -0,0 +1,8 @@
+#ifndef PINS_BOARD_A
+#define PINS_BOARD_A
+
+#define PIN_POT     A1
+#define PIN_LDR     A0
+#define PIN_BUZZER  3
+
+#endif
---
+++ src/PinsModelB.h
@@ -0,0 +1,8 @@
+#ifndef PINS_BOARD_B
+#define PINS_BOARD_B
+
+#define PIN_POT     A7
+#define PIN_LDR     A6
+#define PIN_BUZZER  3
+
+#endif
