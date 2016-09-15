9deef6c16df4db9e4eae086cd027d1cf200e5b1c
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-09-15 12:14:01 +0200

    Added new class for the model B of the board

---
+++ src/NanoPlayBoardB.cpp
@@ -0,0 +1,15 @@
+/*
+  NanoPlayBoardB.cpp - Library for the Arduino Nano PlayBoard
+  Created by José Juan Sánchez, September 15, 2016.
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include "NanoPlayBoardB.h"
+
+NanoPlayBoardB::NanoPlayBoardB()
+{
+    potentiometer = Potentiometer(PIN_POT);
+    ldr = LDR(PIN_LDR);
+    buzzer = Buzzer(PIN_BUZZER);
+}
---
+++ src/NanoPlayBoardB.h
@@ -0,0 +1,19 @@
+/*
+  NanoPlayBoardB.h - Library for the Arduino Nano PlayBoard
+  Created by José Juan Sánchez, September 15, 2016.
+  Released into the public domain.
+*/
+
+#ifndef NanoPlayBoardB_h
+#define NanoPlayBoardB_h
+
+#include "NanoPlayBoard.h"
+#include "PinsModelB.h"
+
+class NanoPlayBoardB: public NanoPlayBoard
+{
+  public:
+    NanoPlayBoardB();
+};
+
+#endif
