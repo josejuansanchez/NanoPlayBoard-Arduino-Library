9a6f31963772f58488dc68510e569ed4cbe86589
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-09-15 12:11:40 +0200

    Renamed constants

--- src/PinsModelA.h
+++ src/PinsModelA.h
@@ -1,8 +1,13 @@
-#ifndef PINS_BOARD_A
-#define PINS_BOARD_A
+#ifndef PINS_MODEL_A
+#define PINS_MODEL_A
 
+#undef PIN_POT
 #define PIN_POT     A1
+
+#undef PIN_LDR
 #define PIN_LDR     A0
+
+#undef PIN_BUZZER
 #define PIN_BUZZER  3
 
 #endif
--- src/PinsModelB.h
+++ src/PinsModelB.h
@@ -1,8 +1,13 @@
-#ifndef PINS_BOARD_B
-#define PINS_BOARD_B
+#ifndef PINS_MODEL_B
+#define PINS_MODEL_B
 
+#undef PIN_POT
 #define PIN_POT     A7
+
+#undef PIN_LDR
 #define PIN_LDR     A6
+
+#undef PIN_BUZZER
 #define PIN_BUZZER  3
 
 #endif
