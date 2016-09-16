ca2b5869ddfbbe998010b515b819bc0bddf538e6
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-09-16 20:19:31 +0200

    Removed tabs

--- src/Register.h
+++ src/Register.h
@@ -11,18 +11,18 @@
 
 class Register
 {
-    public:
-        Register();
-        Register(uint8_t dIn, uint8_t clkIn, uint8_t clkOut);
-        void clear();
-        void write(byte data);
+  public:
+    Register();
+    Register(uint8_t dIn, uint8_t clkIn, uint8_t clkOut);
+    void clear();
+    void write(byte data);
 
-    private:
-        uint8_t _dIn;             // Pin attached to the serial pin
-        uint8_t _clkIn;           // Pin attached to the shift clock
-        uint8_t _clkOut;          // Pin atrached to the latch clock
-        unsigned long _period;
-        unsigned int _baudRate;
+  private:
+    uint8_t _dIn;             // Pin attached to the serial pin
+    uint8_t _clkIn;           // Pin attached to the shift clock
+    uint8_t _clkOut;          // Pin atrached to the latch clock
+    unsigned long _period;
+    unsigned int _baudRate;
 };
 
 #endif
