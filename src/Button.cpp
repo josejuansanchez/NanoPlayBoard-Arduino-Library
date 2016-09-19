1c5118e6215126162c318958d107d0e2935d55cf
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-09-19 12:39:29 +0200

    Added new class for buttons

---
+++ src/Button.cpp
@@ -0,0 +1,46 @@
+/*
+  Button.cpp - Library for the Arduino Nano PlayBoard
+  Created by José Juan Sánchez, September 19, 2016.
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include "Button.h"
+
+Button::Button() {}
+
+Button::Button(uint8_t pin_a0, uint8_t pin_a1, button_type type)
+{
+    _pin_a0 = pin_a0;
+    _pin_a1 = pin_a1;
+    _type = type;
+}
+
+// TODO: Fix a well known issue with the state of the top button.
+// The current version of NanoPlayBoard has a bug and the state of the
+// top button cannot be detected. It will fixed in the next version.
+
+button_type Button::whatButtonIsPressed()
+{
+    uint8_t state_a0 = digitalRead(_pin_a0);
+    uint8_t state_a1 = digitalRead(_pin_a1);
+
+    if (state_a0 == 1) {
+        if (state_a1 == 1) {
+            return TOP;
+        } else {
+            return RIGHT;
+        }
+    } else {
+        if (state_a1 == 0) {
+            return DOWN;
+        } else {
+            return LEFT;
+        }
+    }
+}
+
+bool Button::isPressed()
+{
+    return (whatButtonIsPressed() == _type);
+}
---
+++ src/Button.h
@@ -0,0 +1,30 @@
+/*
+  Button.h - Library for the Arduino Nano PlayBoard
+  Created by José Juan Sánchez, September 19, 2016.
+  Released into the public domain.
+*/
+
+#ifndef Button_h
+#define Button_h
+
+#include "Arduino.h"
+
+enum button_type { TOP, DOWN, LEFT, RIGHT };
+
+class Button
+{
+  public:
+    Button();
+    Button(uint8_t pin_a0, uint8_t pin_a1, button_type type);
+    bool isPressed();
+
+  protected:
+    button_type whatButtonIsPressed();
+
+  private:
+    uint8_t _pin_a0;
+    uint8_t _pin_a1;
+    button_type _type;
+};
+
+#endif
