6e3ac51b5e80b9aa139c153a677d61a10ecf85de
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-09-28 23:54:30 +0200

    Minor improvements and refactored code

--- examples/06.android/firmware_usb_otg/firmware_usb_otg.ino
+++ examples/06.android/firmware_usb_otg/firmware_usb_otg.ino
@@ -3,19 +3,49 @@
 
 NanoPlayBoard board;
 String json;
-int sketchId = -1;
+int sketch_id;
+
+int last_pot_value = -1;
+int last_ldr_value = -1;
+
+//---------------------------------
+
+#define ID_POTENTIOMETER_READ       10
+#define ID_POTENTIOMETER_SCALE_TO   11
+
+#define ID_LDR_READ                 20
+#define ID_LDR_SCALE_TO             21
+
+#define ID_RGB_ON                   30
+#define ID_RGB_OFF                  31
+#define ID_RGB_TOGGLE               32
+#define ID_RGB_SET_COLOR            33
+#define ID_RGB_SET_INTENSITY        34
+
+#define ID_BUZZER_PLAY_TONE         40
+#define ID_BUZZER_STOP_TONE         41
+
+#define ID_LEDMATRIX_PRINT_CHAR     50
+#define ID_LEDMATRIX_PRINT_PATTERN  51
+#define ID_LEDMATRIX_PRINT_STRING   52
+#define ID_LEDMATRIX_PRINT_IN_LAND  53
 
 //---------------------------------
 
 // Functions definition
 int getSketchId();
 void runSketch();
-void loadPotentiometerSketch();
-void loadLDRSketch();
-void loadRGBSketch();
-void loadBuzzerSketch();
-void loadLedMatrixPatternSketch();
-void loadLedMatrixSketch();
+void potentiometerRead();
+void potentiometerScaleTo();
+void ldrRead();
+void ldrScaleTo();
+void rgbOn();
+void rgbOff();
+void rgbToggle();
+void rgbSetColor();
+void buzzerPlayTone();
+void ledmatrixPrintPattern();
+void ledmatrixPrintString();
 
 //---------------------------------
 
@@ -28,8 +58,8 @@ void loop() {
 }
 
 void serialEvent() {
-  int newSketchId = getSketchId();
-  if (newSketchId != -1) sketchId = newSketchId;
+  int new_sketch_id = getSketchId();
+  if (new_sketch_id != -1) sketch_id = new_sketch_id;
 }
 
 //---------------------------------
@@ -46,68 +76,168 @@ int getSketchId() {
     return -1;
   }
 
-  return root["sketchId"];
+  return root["id"];
 }
+
+//---------------------------------
 
 void runSketch() {
-    switch(sketchId) {
-      case 0: loadPotentiometerSketch();
-              break;
+  switch(sketch_id) {
+    case ID_POTENTIOMETER_READ:
+      potentiometerRead();
+      break;
 
-      case 1: loadLDRSketch();
-              break;
+    case ID_POTENTIOMETER_SCALE_TO:
+      potentiometerScaleTo();
+      break;
 
-      case 2: loadRGBSketch();
-              break;
+    case ID_LDR_READ:
+      ldrRead();
+      break;
 
-      case 3: loadBuzzerSketch();
-              break;
+    case ID_LDR_SCALE_TO:
+      ldrScaleTo();
+      break;
 
-      case 4: loadLedMatrixSketch();
-              break;
+    case ID_RGB_ON:
+      rgbOn();
+      break;
 
-      case 5: loadLedMatrixPatternSketch();
-              break;
-    }
+    case ID_RGB_OFF:
+      rgbOff();
+      break;
+
+    case ID_RGB_TOGGLE:
+      rgbToggle();
+      sketch_id = -1;
+      break;
+
+    case ID_RGB_SET_COLOR:
+      rgbSetColor();
+      break;
+
+    case ID_BUZZER_PLAY_TONE:
+      buzzerPlayTone();
+      sketch_id = -1;
+      break;
+
+    case ID_LEDMATRIX_PRINT_PATTERN:
+      ledmatrixPrintPattern();
+      break;
+
+    case ID_LEDMATRIX_PRINT_STRING:
+      ledmatrixPrintString();
+      break;
+  }
 }
 
 //---------------------------------
-int lastPotValue = -1;
-void loadPotentiometerSketch() {
-  int potValue = board.potentiometer.read();
 
-  if (potValue == lastPotValue) return;
-  lastPotValue = potValue;
+void potentiometerRead() {
+  int pot_value = board.potentiometer.read();
 
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  root["potentiometer"] = potValue;
+  if (pot_value == last_pot_value) return;
+  last_pot_value = pot_value;
+
+  StaticJsonBuffer<200> json_buffer;
+  JsonObject& root = json_buffer.createObject();
+  root["potentiometer"] = pot_value;
   root.printTo(Serial);
-  delay(100);
-}
-
-//---------------------------------
-int lastLdrValue = -1;
-void loadLDRSketch() {
-  int ldrValue = board.ldr.read();
-
-  if (ldrValue == lastLdrValue) return;
-  lastLdrValue = ldrValue;
-
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  root["ldr"] = ldrValue;
-  root.printTo(Serial);
-  delay(100);
 }
 
 //---------------------------------
 
-void loadRGBSketch() {
+void potentiometerScaleTo() {
   if (json.length() <= 0) return;
 
-  StaticJsonBuffer<200> jsonBuffer;  
-  JsonObject& root = jsonBuffer.parseObject(json);
+  StaticJsonBuffer<200> json_buffer_in;
+  JsonObject& root_in = json_buffer_in.parseObject(json);
+
+  if (!root_in.success()) {
+    Serial.println("{\"error\": \"Error parsing json message\"}");
+    return;
+  }
+
+  int to_low = root_in["to_low"];
+  int to_high = root_in["to_high"];
+
+  int pot_value = board.potentiometer.scaleTo(to_low, to_high);
+
+  if (pot_value == last_pot_value) return;
+  last_pot_value = pot_value;
+
+  StaticJsonBuffer<200> json_buffer_out;
+  JsonObject& root_out = json_buffer_out.createObject();
+  root_out["potentiometer"] = pot_value;
+  root_out.printTo(Serial);
+}
+
+//---------------------------------
+
+void ldrRead() {
+  int ldr_value = board.ldr.read();
+
+  if (ldr_value == last_ldr_value) return;
+  last_ldr_value = ldr_value;
+
+  StaticJsonBuffer<200> json_buffer;
+  JsonObject& root = json_buffer.createObject();
+  root["ldr"] = ldr_value;
+  root.printTo(Serial);
+}
+
+//---------------------------------
+
+void ldrScaleTo() {
+  if (json.length() <= 0) return;
+
+  StaticJsonBuffer<200> json_buffer_in;
+  JsonObject& root_in = json_buffer_in.parseObject(json);
+
+  if (!root_in.success()) {
+    Serial.println("{\"error\": \"Error parsing json message\"}");
+    return;
+  }
+
+  int to_low = root_in["to_low"];
+  int to_high = root_in["to_high"];
+
+  int ldr_value = board.ldr.scaleTo(to_low, to_high);
+
+  if (ldr_value == last_ldr_value) return;
+  last_ldr_value = ldr_value;
+
+  StaticJsonBuffer<200> json_buffer_out;
+  JsonObject& root_out = json_buffer_out.createObject();
+  root_out["ldr"] = ldr_value;
+  root_out.printTo(Serial);
+}
+
+//---------------------------------
+
+void rgbOn() {
+  board.rgb.on();
+}
+
+//---------------------------------
+
+void rgbOff() {
+  board.rgb.off();
+}
+
+//---------------------------------
+
+void rgbToggle() {
+  board.rgb.toggle();
+}
+
+//---------------------------------
+
+void rgbSetColor() {
+  if (json.length() <= 0) return;
+
+  StaticJsonBuffer<200> json_buffer;
+  JsonObject& root = json_buffer.parseObject(json);
 
   if (!root.success()) {
     Serial.println("{\"error\": \"Error parsing json message\"}");
@@ -123,11 +253,11 @@ void loadRGBSketch() {
 
 //---------------------------------
 
-void loadBuzzerSketch() {
+void buzzerPlayTone() {
   if (json.length() <= 0) return;
 
-  StaticJsonBuffer<200> jsonBuffer;  
-  JsonObject& root = jsonBuffer.parseObject(json);
+  StaticJsonBuffer<200> json_buffer;
+  JsonObject& root = json_buffer.parseObject(json);
 
   if (!root.success()) {
     Serial.println("{\"error\": \"Error parsing json message\"}");
@@ -142,28 +272,11 @@ void loadBuzzerSketch() {
 
 //---------------------------------
 
-void loadLedMatrixSketch() {
+void ledmatrixPrintPattern() {
   if (json.length() <= 0) return;
   
-  StaticJsonBuffer<200> jsonBuffer;  
-  JsonObject& root = jsonBuffer.parseObject(json);
-
-  if (!root.success()) {
-    Serial.println("{\"error\": \"Error parsing json message\"}");
-    return;
-  }
-
-  String text = root["text"];
-  board.ledmatrix.print(text);  
-}
-
-//---------------------------------
-
-void loadLedMatrixPatternSketch() {
-  if (json.length() <= 0) return;
-  
-  StaticJsonBuffer<200> jsonBuffer;  
-  JsonObject& root = jsonBuffer.parseObject(json);
+  StaticJsonBuffer<200> json_buffer;
+  JsonObject& root = json_buffer.parseObject(json);
 
   if (!root.success()) {
     Serial.println("{\"error\": \"Error parsing json message\"}");
@@ -179,3 +292,22 @@ void loadLedMatrixPatternSketch() {
 
   board.ledmatrix.print(pattern);
 }
+
+//---------------------------------
+
+void ledmatrixPrintString() {
+  if (json.length() <= 0) return;
+  
+  StaticJsonBuffer<200> json_buffer;
+  JsonObject& root = json_buffer.parseObject(json);
+
+  if (!root.success()) {
+    Serial.println("{\"error\": \"Error parsing json message\"}");
+    return;
+  }
+
+  String text = root["text"];
+  board.ledmatrix.print(text);
+}
+
+//---------------------------------
