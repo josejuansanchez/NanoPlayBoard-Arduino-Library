040a008bbbe7c579ee89398d58b5e51fc4ab7859
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-10-10 12:06:42 +0200

    Fixed minor issue

--- examples/07.servos/robot_02/robot_02.ino
+++ examples/07.servos/robot_02/robot_02.ino
@@ -14,8 +14,8 @@ void loop() {
 
   if (distance < MAX_DISTANCE_FROM_OBSTACLE && distance !=0) {
     board.servos.stop();
-    //board.servos.goBackward(1000);
-    //board.servos.goRight(1000);
+    board.servos.goBackward(1000);
+    board.servos.goRight(1000);
   } else {
     board.servos.goForward();
   }
