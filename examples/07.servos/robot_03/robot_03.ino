c766af1e657ae65e68c2afda3c21fbd7baea6dcf
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-10-10 12:09:30 +0200

    Added improved version of the basic obstacle avoiding robot

---
+++ examples/07.servos/robot_03/robot_03.ino
@@ -0,0 +1,25 @@
+#include <NanoPlayBoard.h>
+
+#define MAX_DISTANCE_FROM_OBSTACLE 10
+
+NanoPlayBoard board;
+
+void setup() {
+
+}
+
+void loop() {
+  int speed = board.potentiometer.scaleTo(0, 100);
+  board.servos.setSpeed(speed);
+  
+  int distance = board.ultrasound.pingCm();
+  board.ledmatrix.printInLandscape(distance);
+
+  if (distance < MAX_DISTANCE_FROM_OBSTACLE && distance !=0) {
+    board.servos.stop();
+    board.servos.goBackward(1000);
+    board.servos.goRight(1000);
+  } else {
+    board.servos.goForward();
+  }
+}
